8320ee97fb987663d5850da6c2d15abf3bf56b20
simon <simon@djobnet.com>
2025-03-25 17:32:53 -0400

    Initial commit

---
+++ src/app/(admin)/(others-pages)/calendar/page.tsx
@@ -0,0 +1,19 @@
+import Calendar from "@/components/calendar/Calendar";
+import PageBreadcrumb from "@/components/common/PageBreadCrumb";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Calendar | Homebor",
+  description:
+    "This is the Calender page for Homebor platform",
+  // other metadata
+};
+export default function page() {
+  return (
+    <div>
+      <PageBreadcrumb pageTitle="Calendar" />
+      <Calendar />
+    </div>
+  );
+}
